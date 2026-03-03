4d13cde1f10ce0a757fd3e5156de99c01087a4e3
kr8907-max <kr8907@srmist.edu.in>
2026-03-03 23:52:38 +0530

    UC2 - Added ASCII OOPS Banner

---
+++ OOPSBannerApp_UGC2.java
@@ -0,0 +1,21 @@
+/**
+ * UC2 - OOPS ASCII Banner Application
+ * Displays OOPS using * and spaces in 7-line grid
+ * @auther Khushi
+ * @version 1.0
+ */
+
+public class OOPSBannerApp_UGC2{
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****    ***** ");
+        System.out.println("*     * *     * *     *  *      ");
+        System.out.println("*     * *     * *     *  *      ");
+        System.out.println("*     * *     * *****    *****  ");
+        System.out.println("*     * *     * *              *");
+        System.out.println("*     * *     * *              *");
+        System.out.println(" *****   *****   *         *****");
+
+    }
+}
